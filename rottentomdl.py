55d093701e1b9a7032e5630c15335b5ed38c4c5e
quentin-dev <q.barbarat.dev@gmail.com>
2016-08-09 15:08:53 +0200

    Uploaded rottentomdl.py  v. 0.0.2

--- rottentomdl.py
+++ rottentomdl.py
@@ -1,14 +1,21 @@
 import urllib.request
-import requests
 
-movieName = str(input("What movie review do you want to download ? "))
+print("-- Thanks for downloading rottentomdl.py V:0.0.2")
+print("-- When searching use _ instead of spaces and lowercase letters")
+print("-- i.e. gone_baby_gone ")
 
-url = "https://www.rottentomatoes.com/m/" + movieName
+pageValidation = False
+while pageValidation == False:
+    try :
+        movieName = str(input("What movie review do you want to download ? "))
+        url = "https://www.rottentomatoes.com/m/" + movieName
+        response = urllib.request.urlopen(url)
+        webContent = str(response.read().decode("utf-8"))
+        pageValidation = True
+    except Exception:
+        pageValidation = False
 
-response = urllib.request.urlopen(url)
-webContent = str(response.read().decode("utf-8"))
-
-dlname = movieName + ".html"
+dlname = movieName + "_rottentomatoes.html"
 f = open(dlname, 'w')
 f.write(webContent)
 f.close
